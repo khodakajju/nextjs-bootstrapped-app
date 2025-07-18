fb5479f781b9b329333c275285314c8310b19cc1
khodakajju <kajjukhoda@gmail.com>
2025-07-18 23:56:30 +0530

    Add .next/static/chunks/src_app_page_tsx_51969a0e._.js 1752863187197

---
+++ .next/static/chunks/src_app_page_tsx_51969a0e._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d3464965._.js",
+  "static/chunks/node_modules_6dd742b4._.js"
+],
+    source: "dynamic"
+});
